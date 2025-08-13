e687bb274b50a0f9d88bf9b2361a367573010624
Arpita Patra <arpitapatra595@gmail.com>
2025-08-13 09:21:15 +0530

    new file

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main{
+    public static void main(String[] args){
+        System.out.println("Arpita Patra");
+        System.out.println("Age:18");
+        System.out.println("University roll no:2415000330");
+    }
+}
